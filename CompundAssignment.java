618593e984213f41660d20c852ca5643e31b1da8
Amrit Raj <amritraj1999a@gmail.com>
2022-02-09 13:21:48 +0530

    new

---
+++ CompundAssignment.java
@@ -0,0 +1,14 @@
+public class CompundAssignment {
+    public static void main(String[] args) {
+        /*
+         * int Result = 8;
+         * Result += 2345;
+         * Result /= 3;
+         * Result %= 5;
+         * Result *= 5;
+         */
+        int result = ((((8 + 2345) / 3) % 5) * 5);
+        System.out.println(Result);
+    }
+
+}
---
+++ Conditional.java
@@ -0,0 +1,15 @@
+public class Conditional {
+    public static void main(String[] args) {
+        int a = 55;
+        int b = 70;
+        /*
+         * if (a < b || a < 55) {
+         * System.out.println("true");
+         * } else {
+         * System.out.println("false");
+         * }
+         */
+        System.out.println((a < b || a < 55));
+    }
+
+}
