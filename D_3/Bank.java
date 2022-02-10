8f125cbd1897878e57bd6fbd480656c5ab79bd9f
Amrit Raj <amritraj1999a@gmail.com>
2022-02-10 18:33:59 +0530

    after creating method

--- D_3/Bank.java
+++ D_3/Bank.java
@@ -1,6 +1,22 @@
-protected class Account {
-    float balance;
+class Account {
+    double balance;
     String accountHoldername;
+
+    protected void debit(int amt) {
+        if (amt > balance) {
+            System.out.println("Debit amount exceeded account balance!");
+        } else {
+            balance -= amt;
+            System.out.println(accountHoldername);
+            System.out.println("Balance:" + balance);
+        }
+    }
+
+    protected void credit(int amt) {
+        balance += amt;
+        System.out.println(accountHoldername);
+        System.out.println("credited by:" + amt + " " + "New balance:" + balance);
+    }
 
 }
 
@@ -12,6 +28,11 @@ public class Bank {
         Account person2 = new Account();
         person2.balance = 888888;
         person2.accountHoldername = "Client2";
+
+        person2.credit(1000);
+        person2.debit(1000);
+        person1.credit(1000);
+        person1.debit(1000);
 
     }
 
